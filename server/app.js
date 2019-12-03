04b93c222816eb52c0e51160ec9c8a8e87038002
Leon <leon.karho.chung@gmail.com>
2019-12-03 15:05:46 -0500

    connect to mongodb atlas with env vars

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -16,3 +18,15 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`**** SERVER LISTENING ON http://localhost:${port} ****`);
 });
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("**** Database connected ****");
+  })
+  .catch(error => {
+    console.error(error);
+  });
