eb544ac823cd44ae9b0baba44074d81d57805351
Leon <leon.karho.chung@gmail.com>
2019-12-05 13:11:55 -0500

    add documentation, import user routes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
+// IMPORTS
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+// ROUTES IMPORT
+const userRoutes = require("./routes/users");
+
+// INIT APP
 const app = express();
 
+// USE MIDDLEWARES
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// USE ROUTES
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   return res.status(200).json({
@@ -14,6 +24,7 @@ app.get("/", (req, res) => {
   });
 });
 
+// LISTEN SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`**** SERVER LISTENING ON http://localhost:${port} ****`);
@@ -22,7 +33,8 @@ app.listen(port, () => {
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
   .then(() => {
     console.log("**** Database connected ****");
