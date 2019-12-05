30d060cc34aeec258187cf39724e802aeca12bf3
Leon <leon.karho.chung@gmail.com>
2019-12-05 14:48:14 -0500

    hash pw and compare pw

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const securePin = require("secure-pin");
+const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 
 // @access  Public
@@ -16,64 +17,78 @@ router.get("/test", (req, res) => {
 // @desc    Create user account
 // @route   POST /create
 router.post("/create", async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  // find user
-  const user = await User.findOne({ email });
+    // find user
+    const user = await User.findOne({ email });
 
-  // if exists - return error
-  if (user) {
-    return res.status(409).json({
-      error: "Email is already in use"
+    // if exists - return error
+    if (user) {
+      return res.status(409).json({
+        error: "Email is already in use"
+      });
+    }
+
+    // generate pin
+    const pin = securePin.generatePinSync(4);
+
+    // gen salt & hash password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    // if false - create user
+    const newUser = new User({
+      email,
+      password: hash,
+      pin
     });
+
+    // save user creation
+    await newUser.save();
+
+    // return data
+    return res.status(201).json({
+      success: true,
+      data: newUser
+    });
+  } catch (error) {
+    console.error(error);
   }
-
-  // generate pin
-  const pin = securePin.generatePinSync(4);
-
-  // if false - create user
-  const newUser = new User({
-    email,
-    password,
-    pin
-  });
-
-  // save user creation
-  await newUser.save();
-
-  // return data
-  return res.status(201).json({
-    success: true,
-    data: newUser
-  });
 });
 
 // @access  Public
 // @desc    Log user account
 // @route   POST /login
 router.post("/login", async (req, res) => {
-  const { pin, password } = req.body;
-  // find user by pin
-  const user = await User.findOne({ pin });
-  // if no user - return error
-  if (!user) {
-    return res.status(404).json({
-      error: "User not found"
-    });
-  } else {
-    // else - compare passwords
-    // if no match - return error
-    if (!(user.password === password)) {
-      return res.status(403).json({
-        error: "Incorrect password"
+  try {
+    const { pin, password } = req.body;
+    // find user by pin
+    const user = await User.findOne({ pin });
+    // if no user - return error
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found"
       });
     } else {
-      // else - login
-      return res.status(200).json({
-        success: true,
-        data: user
-      });
+      // else - compare passwords
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // if no match - return error
+      if (!isMatch) {
+        return res.status(403).json({
+          error: "Incorrect password"
+        });
+      } else {
+        // else - login
+        return res.status(200).json({
+          success: true,
+          data: user
+        });
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
 });
 
