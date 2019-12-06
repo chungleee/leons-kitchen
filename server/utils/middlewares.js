507753ae6e9ac8314d4631cf73e5b144dc129b78
Leon <leon.karho.chung@gmail.com>
2019-12-06 15:07:06 -0500

    errors obj, check if headers are set

--- server/utils/middlewares.js
+++ server/utils/middlewares.js
@@ -1,16 +1,30 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
+const errors = {
+  logIn: "Please log in.",
+  unauthorized: "Access unauthorized"
+};
+
 const authenticate = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (
+      !(
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer ")
+      )
+    ) {
+      return res.status(401).json({ error: errors.logIn });
+    } else {
+      const token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log("decoded", decoded);
-    const user = await User.findById({ _id: decoded._id });
-    req.user = user;
-    res.locals.user_role = decoded.role;
-    next();
+      console.log("decoded", decoded);
+      const user = await User.findById({ _id: decoded._id });
+      req.user = user;
+      res.locals.user_role = decoded.role;
+      next();
+    }
   } catch (error) {
     console.error(error);
     return res.status(403).json(error);
@@ -21,7 +35,7 @@ const checkRole = role => async (req, res, next) => {
   try {
     if (role !== res.locals.user_role) {
       return res.status(401).json({
-        error: "Access unauthorized"
+        error: errors.unauthorized
       });
     } else {
       next();
