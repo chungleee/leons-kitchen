dd5f72a0b1bf61308ed587d3d1cf7c3294c9ee78
Leon <leon.karho.chung@gmail.com>
2019-12-07 16:35:38 -0500

    Order model schema

---
+++ server/models/orderModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+  {
+    food_items: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Food",
+        required: true
+      }
+    ],
+    price_total: {
+      type: String,
+      required: true
+    },
+    order_id: {
+      type: String,
+      required: true
+    },
+    order_for: {
+      type: String,
+      required: true
+    },
+    payment_type: {
+      type: String,
+      required: true
+    },
+    order_owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    timestamps
+  }
+);
+
+module.exports = Order = mongoose.model("Order", orderSchema);
