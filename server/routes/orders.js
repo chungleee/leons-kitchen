07530399c49f6c1ee519de1b485b5bdf2837576e
Leon <leon.karho.chung@gmail.com>
2019-12-07 18:01:27 -0500

    fetch all orders, fetch order by id, delete order by id

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -48,4 +48,59 @@ router.post("/create", async (req, res) => {
   }
 });
 
+// @access  Public - for admins
+// @desc    Get all orders
+// @route   GET /
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    if (!orders) {
+      return res.status(404).json({ error: "Orders not found" });
+    } else {
+      return res.status(200).json({ success: true, data: orders });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ msg: "Something went wrong", error });
+  }
+});
+
+// @access  Public - for admins
+// @desc    Get order by id
+// @route   GET /orderId
+router.get("/:orderId", async (req, res) => {
+  try {
+    const _id = req.params.orderId;
+    const order = await Order.findById({ _id }).populate(
+      "food_items order_owner"
+    );
+    if (!order) {
+      return res.status(404).json({ error: "Order not found" });
+    } else {
+      return res.status(200).json({ success: true, data: order });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json(error);
+  }
+});
+
+// @access  Public - for staffs
+// @desc    Delete order
+// @route   DELETE /orderId
+router.delete("/:orderId", async (req, res) => {
+  try {
+    const _id = req.params.orderId;
+    const orderToDelete = await Order.findByIdAndDelete({ _id });
+    if (!orderToDelete) {
+      return res.status(404).json({ error: "Order not found." });
+    } else {
+      return res.status(200).json({ deleted: true, data: orderToDelete });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json(error);
+  }
+});
+
 module.exports = router;
