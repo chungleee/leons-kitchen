9ccd7817af38d9327d43517b7db176a62876cc15
Leon <leon.karho.chung@gmail.com>
2019-12-10 16:24:04 -0500

    theme palette

---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  background: "#fffffe",
+  text: "#2d334a",
+  color: {
+    primary: "#fffffe",
+    secondary: "#e3f6f5",
+    tertiary: "#bae8e8",
+    highlight: "#ffd803"
+  }
+};
+
+export default theme;
