635b470c8cbf323ea2a1b0e49b1a8f13cb99e13e
Leon <leon.karho.chung@gmail.com>
2019-12-10 17:13:48 -0500

    placeholder order menu component

---
+++ client/src/components/pages/OrderMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OrderMenu = props => {
+  return (
+    <div>
+      this is the order menu where user orders food - for staffs and (future
+      users)
+    </div>
+  );
+};
+
+export default OrderMenu;
