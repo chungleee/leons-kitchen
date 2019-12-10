af12202a7246f669660ccc60ddd768c81a1dc49b
Leon <leon.karho.chung@gmail.com>
2019-12-10 16:38:18 -0500

    compose reusable input field

---
+++ client/src/components/presentationals/InputField.js
@@ -0,0 +1,32 @@
+/** @jsx jsx */
+import React from "react";
+import { css, jsx } from "@emotion/core";
+import theme from "../../theme";
+
+const InputField = ({ type, name, value, onChange }) => {
+  return (
+    <input
+      css={{
+        color: `${theme.text}`,
+        height: "1.5rem",
+        border: `1px solid ${theme.text}`,
+        fontSize: "100%",
+        lineHeight: 1.15,
+        padding: "0.5rem",
+        marginBottom: "1rem",
+        "&:hover": {
+          backgroundColor: `${theme.color.highlight}`
+        },
+        "&:focus": {
+          outline: "none"
+        }
+      }}
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default InputField;
