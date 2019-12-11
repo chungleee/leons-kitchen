06522e3acbf94c0b22a54c2c92e9d2693be9b7d1
Leon <leon.karho.chung@gmail.com>
2019-12-11 15:45:21 -0500

    more route setups

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/pages/Login";
 import AdminDashboard from "./components/pages/AdminDashboard";
+import OrderMenu from "./components/pages/OrderMenu";
+import ErrorPage from "./components/pages/ErrorPage";
 
 function App() {
+  const [user, setUser] = useState({ name: "leon", role: "staff" });
   return (
     <Router>
-      <Route
-        exact
-        path="/"
-        render={() => {
-          return <Login />;
-        }}
-      />
-      <Route
-        path="/admin"
-        render={() => {
-          return <AdminDashboard user={{ name: "admin", role: "staff" }} />;
-        }}
-      />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <Login />;
+          }}
+        />
+        <Route
+          path="/admin"
+          render={() => {
+            return <AdminDashboard user={user} />;
+          }}
+        />
+        <Route
+          path="/order-menu"
+          render={() => {
+            return <OrderMenu user={user} />;
+          }}
+        />
+
+        <Route component={ErrorPage} />
+      </Switch>
     </Router>
   );
 }
