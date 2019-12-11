c8eed96d15ba2c54481783d861e266737864045e
Leon <leon.karho.chung@gmail.com>
2019-12-11 16:04:13 -0500

    redux boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import "./index.css";
 import App from "./App";
+import rootReducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const middlewares = [thunk];
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
