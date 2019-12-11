cacd38fa515214016c79bcd5e3e3da9279bcf7e2
Leon <leon.karho.chung@gmail.com>
2019-12-11 17:56:41 -0500

    remove password field from returning

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,27 +1,41 @@
 const mongoose = require("mongoose");
 
 const ROLES = ["staff", "manager", "admin"];
-const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    pin: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false
+    },
+    role: {
+      type: String,
+      enum: ROLES,
+      default: "staff"
+    }
   },
-  pin: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false
-  },
-  role: {
-    type: String,
-    enum: ROLES,
-    default: "staff"
+  {
+    toObject: {
+      transform: function(doc, ret) {
+        delete ret.password;
+      }
+    },
+    toJSON: {
+      transform: function(doc, ret) {
+        delete ret.password;
+      }
+    }
   }
-});
+);
 
 module.exports = User = mongoose.model("User", userSchema);
