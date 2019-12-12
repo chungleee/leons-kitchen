4a9bc35f18b9e9f35525c20f3c11f1599fd392d9
Leon <leon.karho.chung@gmail.com>
2019-12-12 12:54:30 -0500

    renaming

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
-  user: userReducer
+  userState: userReducer
 });
 
 export default rootReducer;
