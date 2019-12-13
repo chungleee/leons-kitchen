1478d192cac8589dcfe04a1f28c5109d6ba93e49
Leon <leon.karho.chung@gmail.com>
2019-12-13 13:24:18 -0500

    when app mounts, check if user authenticated, redirect accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,40 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  useHistory
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 import Login from "./components/pages/Login";
 import AdminDashboard from "./components/pages/AdminDashboard";
 import OrderMenu from "./components/pages/OrderMenu";
 import ErrorPage from "./components/pages/ErrorPage";
 
 function App() {
-  const [user, setUser] = useState({ name: "leon", role: "staff" });
+  const { isAuthenticated, user } = useSelector(state => {
+    return state.userState;
+  });
+
   return (
     <Router>
       <Switch>
+        <Route exact path="/">
+          {!isAuthenticated ? <Redirect to="/login" /> : null}
+        </Route>
+
         <Route
-          exact
-          path="/"
+          path="/login"
           render={() => {
             return <Login />;
           }}
         />
+
         <Route
           path="/admin"
           render={() => {
             return <AdminDashboard user={user} />;
-          }}
-        />
-        <Route
-          path="/order-menu"
-          render={() => {
-            return <OrderMenu user={user} />;
           }}
         />
 
