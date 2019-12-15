2f32de522589e55df4f8e1adb194ccadb46b8c02
Leon <leon.karho.chung@gmail.com>
2019-12-15 17:31:44 -0500

    moved

--- client/src/components/pages/AdminDashboard.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CheckAuth from "../HOC/CheckAuth";
-
-const AdminDashboard = props => {
-  return <div>this is the admin dashboard - only for admins</div>;
-};
-
-export default CheckAuth(AdminDashboard, ["admin"]);
