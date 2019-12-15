8e39095e95da919081fad2d1afef18ef4db21ac8
Leon <leon.karho.chung@gmail.com>
2019-12-15 18:14:26 -0500

    navbar and styling

--- client/src/components/pages/admin/AdminDashboard.js
+++ client/src/components/pages/admin/AdminDashboard.js
@@ -2,41 +2,30 @@
 import React from "react";
 import { jsx } from "@emotion/core";
 import theme from "../../../theme";
-import { NavLink } from "react-router-dom";
 import CheckAuth from "../../HOC/CheckAuth";
+import Navbar from "./presentations/Navbar";
+
+const styles = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    "& a": {
+      textDecoration: "none",
+      color: theme.text
+    }
+  },
+  main: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: theme.color.highlight
+  }
+};
 
 const AdminDashboard = props => {
   return (
-    <div
-      css={{
-        height: "100vh",
-        display: "flex"
-      }}
-    >
-      <aside
-        css={{
-          height: "100%",
-          width: "33%",
-          backgroundColor: theme.color.secondary
-        }}
-      >
-        <div>Leon's Kitchen</div>
-        <ul>
-          <li>
-            <NavLink to="/admin/employees">Employees</NavLink>
-          </li>
-          <li>
-            <NavLink to="/admin/food-menu">Food menu</NavLink>
-          </li>
-        </ul>
-      </aside>
-      <main
-        css={{
-          height: "100%",
-          width: "100%",
-          backgroundColor: theme.color.highlight
-        }}
-      >
+    <div css={styles.container}>
+      <Navbar />
+      <main css={styles.main}>
         <h1>something</h1>
       </main>
     </div>
