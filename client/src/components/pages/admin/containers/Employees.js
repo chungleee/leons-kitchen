159da5f48edc69a1b0cf5da916dd3f0c00f87c12
Leon <leon.karho.chung@gmail.com>
2019-12-16 15:01:54 -0500

    employees page: responsible for display list of employees and dynamic route components

--- client/src/components/pages/admin/containers/Employees.js
+++ client/src/components/pages/admin/containers/Employees.js
@@ -1,9 +1,30 @@
+/** @jsx jsx */
 import React from "react";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import { jsx } from "@emotion/core";
+import theme from "../../../../theme";
+import EmployeeList from "../presentations/EmployeeList";
+import CreateEmployee from "./CreateEmployee";
+
+const dummies = [
+  { id: 1, name: "leon chung", role: "staff" },
+  { id: 2, name: "john doe", role: "staff" },
+  { id: 3, name: "jane doe", role: "staff" }
+];
 
 const Employees = () => {
   return (
-    <div>
-      <p>this is the employees component</p>
+    <div
+      css={{
+        display: "flex",
+        height: "100%"
+      }}
+    >
+      <EmployeeList dummies={dummies} />
+      <main css={{ padding: "3rem" }}>
+        <Route path="/admin/employees/create" component={CreateEmployee} />
+        <Route path="/admin/employees/:userId" component={CreateEmployee} />
+      </main>
     </div>
   );
 };
