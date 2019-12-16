a4afbc9ed1469a19196d9604c4e57230d7386f8b
Leon <leon.karho.chung@gmail.com>
2019-12-16 17:07:48 -0500

    add Switch + styling

--- client/src/components/pages/admin/containers/Employees.js
+++ client/src/components/pages/admin/containers/Employees.js
@@ -1,10 +1,11 @@
 /** @jsx jsx */
 import React from "react";
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { jsx } from "@emotion/core";
 import theme from "../../../../theme";
 import EmployeeList from "../presentations/EmployeeList";
 import CreateEmployee from "./CreateEmployee";
+import EmployeeDetail from "./EmployeeDetail";
 
 const dummies = [
   { id: 1, name: "leon chung", role: "staff" },
@@ -21,9 +22,17 @@ const Employees = () => {
       }}
     >
       <EmployeeList dummies={dummies} />
-      <main css={{ padding: "3rem" }}>
-        <Route path="/admin/employees/create" component={CreateEmployee} />
-        <Route path="/admin/employees/:userId" component={CreateEmployee} />
+      <main
+        css={{
+          padding: "2rem",
+          width: "100%",
+          backgroundColor: `${theme.color.secondary}`
+        }}
+      >
+        <Switch>
+          <Route path="/admin/employees/create" component={CreateEmployee} />
+          <Route path="/admin/employees/:userId" component={EmployeeDetail} />
+        </Switch>
       </main>
     </div>
   );
