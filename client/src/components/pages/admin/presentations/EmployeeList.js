26698d6e25f03b1d0b9329817d13cec1cfa49c44
Leon <leon.karho.chung@gmail.com>
2019-12-16 17:08:23 -0500

    styling + return navlinks with state passed in as props

--- client/src/components/pages/admin/presentations/EmployeeList.js
+++ client/src/components/pages/admin/presentations/EmployeeList.js
@@ -4,50 +4,53 @@ import { NavLink } from "react-router-dom";
 import { jsx } from "@emotion/core";
 import theme from "../../../../theme";
 
+const styles = {
+  wrapper: {
+    width: "25%",
+    backgroundColor: theme.color.primary,
+    borderRight: "1px solid lightgrey",
+    paddingTop: "2rem"
+  },
+  ul: {
+    width: "70%",
+    margin: "auto"
+  },
+  navlinks: {
+    normal: {
+      display: "block",
+      marginTop: "0.5rem",
+      padding: "1rem"
+    },
+    active: {
+      border: `1px solid ${theme.color.highlight}`
+    }
+  },
+  p: { fontSize: "1.2rem" }
+};
+
 const EmployeeList = ({ dummies }) => {
   return (
-    <aside
-      css={{
-        width: "25%",
-        backgroundColor: theme.color.primary,
-        borderRight: "1px solid lightgrey",
-        paddingTop: "3rem"
-      }}
-    >
-      <ul
-        css={{
-          width: "70%",
-          margin: "auto"
-        }}
-      >
+    <aside css={styles.wrapper}>
+      <ul css={styles.ul}>
         <NavLink
-          css={{
-            display: "block",
-            marginTop: "0.5rem",
-            padding: "1rem"
-          }}
-          activeStyle={{
-            border: `1px solid ${theme.color.highlight}`
-          }}
+          css={styles.navlinks.normal}
+          activeStyle={styles.navlinks.active}
           to="/admin/employees/create"
         >
-          Add employee
+          Create employee
         </NavLink>
         {dummies.map(dummy => {
           return (
             <NavLink
-              css={{
-                display: "block",
-                marginTop: "0.5rem",
-                padding: "1rem"
-              }}
-              activeStyle={{
-                border: `1px solid ${theme.color.highlight}`
-              }}
+              css={styles.navlinks.normal}
+              activeStyle={styles.navlinks.active}
               key={dummy.id}
-              to={`/admin/employees/${dummy.id}`}
+              to={{
+                pathname: `/admin/employees/${dummy.id}`,
+                state: dummy
+              }}
             >
-              <p css={{ fontSize: "1.2rem" }}>{dummy.name}</p>
+              <p css={styles.p}>{dummy.name}</p>
               <small>{dummy.role}</small>
             </NavLink>
           );
