a2fbddd8fd6276f3b690cb281d3f5363bc5c682d
Leon <leon.karho.chung@gmail.com>
2019-12-17 14:00:33 -0500

    add loading state, dispatching

--- client/src/components/pages/admin/containers/Employees.js
+++ client/src/components/pages/admin/containers/Employees.js
@@ -1,8 +1,11 @@
 /** @jsx jsx */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { handleFetchEmployees } from "../../../../redux/actions/employeesActions";
 import { jsx } from "@emotion/core";
 import theme from "../../../../theme";
+import Spinner from "../../../common/Spinner/Spinner";
 import EmployeeList from "../presentations/EmployeeList";
 import CreateEmployee from "./CreateEmployee";
 import EmployeeDetail from "./EmployeeDetail";
@@ -14,6 +17,31 @@ const dummies = [
 ];
 
 const Employees = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const { employees } = useSelector(state => {
+    return state.employeesState;
+  });
+  useEffect(() => {
+    dispatch(handleFetchEmployees());
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        css={{
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+          height: "100%"
+        }}
+      >
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div
       css={{
@@ -21,7 +49,7 @@ const Employees = () => {
         height: "100%"
       }}
     >
-      <EmployeeList dummies={dummies} />
+      <EmployeeList dummies={dummies} employees={employees} />
       <main
         css={{
           padding: "2rem",
