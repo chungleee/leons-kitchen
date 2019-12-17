33035aaa3a2fb83046bb2ae001aaa9d6bbe1c267
Leon <leon.karho.chung@gmail.com>
2019-12-17 14:02:01 -0500

    add fields

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,6 +3,14 @@ const mongoose = require("mongoose");
 const ROLES = ["staff", "kitchen", "admin"];
 const userSchema = mongoose.Schema(
   {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
     email: {
       type: String,
       required: true,
