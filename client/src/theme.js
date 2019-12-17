8370699b7db43180d55f7cc2a823131895762dfb
Leon <leon.karho.chung@gmail.com>
2019-12-17 12:04:14 -0500

    add error theme color

--- client/src/theme.js
+++ client/src/theme.js
@@ -3,6 +3,7 @@ const black = "#2d334a";
 const lightTeal = "#e3f6f5";
 const darkTeal = "#bae8e8";
 const yellow = "#ffd803";
+const red = "#f25042";
 
 const theme = {
   background: white,
@@ -11,7 +12,8 @@ const theme = {
     primary: white,
     secondary: lightTeal,
     tertiary: darkTeal,
-    highlight: yellow
+    highlight: yellow,
+    danger: red
   }
 };
 
