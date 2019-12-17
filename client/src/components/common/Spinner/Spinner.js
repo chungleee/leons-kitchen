071b39826cfef226e174c44f5600be46cfeb903b
Leon <leon.karho.chung@gmail.com>
2019-12-17 13:56:30 -0500

    spinner component

---
+++ client/src/components/common/Spinner/Spinner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-bean-eater-34ph8wxdizj">
+      <div className="ldio-73rna5jl916">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
