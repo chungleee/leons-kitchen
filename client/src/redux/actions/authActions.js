da232298c67854248030b9d18464daab8415df4e
Leon <leon.karho.chung@gmail.com>
2019-12-17 13:56:52 -0500

    rename file

---
+++ client/src/redux/actions/authActions.js
@@ -0,0 +1,30 @@
+import { USER_LOGIN, CURRENT_USER } from "./types";
+import axios from "axios";
+
+export const handleUserLogin = credentials => {
+  return async dispatch => {
+    try {
+      const res = await axios.post("/api/users/login", credentials);
+
+      if (window.localStorage) {
+        localStorage.setItem("leon's kitchen jwtToken", res.data.token);
+      }
+
+      dispatch({
+        type: USER_LOGIN,
+        payload: res.data.user
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const handleCurrentUser = decoded => {
+  return dispatch => {
+    dispatch({
+      type: CURRENT_USER,
+      payload: decoded
+    });
+  };
+};
