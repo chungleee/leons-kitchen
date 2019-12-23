0abb526be9b7e56bf535d3b79b639b3c95eae5f5
Leon <leon.karho.chung@gmail.com>
2019-12-23 15:16:35 -0500

    pass all props

--- client/src/components/HOC/CheckAuth.js
+++ client/src/components/HOC/CheckAuth.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const CheckAuth = (Component, rolesArray) => {
-  return ({ user }) => {
-    if (rolesArray.includes(user.role)) {
-      return <Component user={user} />;
+  return props => {
+    if (rolesArray.includes(props.user.role)) {
+      return <Component user={props.user} {...props} />;
     }
 
     return <h1>You don't have access to this page</h1>;
