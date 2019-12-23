b30f8e18a67afd1a365557dd3763fadd963b9570
Leon <leon.karho.chung@gmail.com>
2019-12-23 15:22:41 -0500

    destructure props

--- client/src/components/HOC/CheckAuth.js
+++ client/src/components/HOC/CheckAuth.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const CheckAuth = (Component, rolesArray) => {
-  return props => {
-    if (rolesArray.includes(props.user.role)) {
-      return <Component user={props.user} {...props} />;
+  return ({ user, ...props }) => {
+    if (rolesArray.includes(user.role)) {
+      return <Component user={user} {...props} />;
     }
 
     return <h1>You don't have access to this page</h1>;
