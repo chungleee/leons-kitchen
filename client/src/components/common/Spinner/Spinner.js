3561ce06d231ae18269e26a73c702c5e475d39c5
Leon <leon.karho.chung@gmail.com>
2019-12-23 14:13:58 -0500

    styling

--- client/src/components/common/Spinner/Spinner.js
+++ client/src/components/common/Spinner/Spinner.js
@@ -3,17 +3,26 @@ import "./spinner.css";
 
 const Spinner = () => {
   return (
-    <div className="loadingio-spinner-bean-eater-34ph8wxdizj">
-      <div className="ldio-73rna5jl916">
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+    <div
+      css={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center",
+        height: "100%"
+      }}
+    >
+      <div className="loadingio-spinner-bean-eater-34ph8wxdizj">
+        <div className="ldio-73rna5jl916">
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
