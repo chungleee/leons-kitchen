bb75f371ff6d226576676d042d61901ae18702a9
Leon <leon.karho.chung@gmail.com>
2019-12-24 14:45:49 -0500

    dummy data

--- client/src/components/pages/admin/containers/food-menu/FoodMenu.js
+++ client/src/components/pages/admin/containers/food-menu/FoodMenu.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 const FoodMenu = props => {
-  return <div>this is the food menu component</div>;
+  const dummies = [
+    {
+      id: 1,
+      title: "general tao",
+      category: "platter",
+      price: "12"
+    },
+    {
+      id: 2,
+      title: "wonton soup",
+      category: "starter",
+      price: "3"
+    },
+    {
+      id: 3,
+      title: "brownies",
+      category: "dessert",
+      price: "5"
+    },
+    {
+      id: 4,
+      title: "coca cola",
+      category: "beverage",
+      price: "2"
+    },
+    {
+      id: 5,
+      title: "gamjatang",
+      category: "custom",
+      price: "12"
+    }
+  ];
+
+  return <div>this is the food list table menu thingy</div>;
 };
 
 export default FoodMenu;
