9e3a97336f5130c8a6da326e7665902d3cb6f5de
Leon <leon.karho.chung@gmail.com>
2019-12-31 17:32:23 -0500

    delete employee handler

--- client/src/redux/actions/employeesActions.js
+++ client/src/redux/actions/employeesActions.js
@@ -1,11 +1,10 @@
-import { FETCH_EMPLOYEES, CREATE_USER } from "./types";
+import { FETCH_EMPLOYEES, CREATE_USER, DELETE_USER } from "./types";
 import axios from "axios";
 
 export const handleCreateEmployee = data => {
   return async dispatch => {
     try {
       const res = await axios.post("/api/users/create", data);
-
       dispatch({
         type: CREATE_USER,
         payload: res.data.newUser
@@ -20,10 +19,24 @@ export const handleFetchEmployees = () => {
   return async dispatch => {
     try {
       const res = await axios.get("/api/users/");
-      console.log("handle fetch users", res.data);
       dispatch({
         type: FETCH_EMPLOYEES,
         payload: res.data.users
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const handleDeleteEmployee = employeeId => {
+  return async dispatch => {
+    try {
+      const res = await axios.delete(`/api/users/${employeeId}`);
+      console.log("delete employee", res.data);
+      dispatch({
+        type: DELETE_USER,
+        payload: res.data.user
       });
     } catch (error) {
       console.error(error);
