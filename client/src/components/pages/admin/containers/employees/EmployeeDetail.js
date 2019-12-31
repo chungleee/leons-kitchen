1c7552d978987dbb457b99befa2d1a862c062ab0
Leon <leon.karho.chung@gmail.com>
2019-12-31 17:31:27 -0500

    unstyled employee details component: can delete employee

--- client/src/components/pages/admin/containers/employees/EmployeeDetail.js
+++ client/src/components/pages/admin/containers/employees/EmployeeDetail.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { handleDeleteEmployee } from "../../../../../redux/actions/employeesActions";
 
-const EmployeeDetail = ({ location }) => {
-  return <div>{JSON.stringify(location.state)}</div>;
+const EmployeeDetail = ({ location, history }) => {
+  const { firstName, lastName, role, email, _id } = location.state.employee;
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h3>{`${firstName} ${lastName}`}</h3>
+      <p>{email}</p>
+      <p>{role}</p>
+      <button
+        onClick={() => {
+          alert("Are you sure you want to delete this employee?");
+          dispatch(handleDeleteEmployee(_id));
+          history.push("/admin/employees");
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default EmployeeDetail;
