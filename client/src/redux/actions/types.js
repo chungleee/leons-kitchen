fd23847939b6d3319d24bb3b9be61dd4aad2e921
Leon <leon.karho.chung@gmail.com>
2019-12-31 17:31:58 -0500

    delete employee const

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -2,3 +2,4 @@ export const USER_LOGIN = "USER_LOGIN";
 export const CURRENT_USER = "CURRENT_USER";
 export const FETCH_EMPLOYEES = "FETCH_EMPLOYEES";
 export const CREATE_USER = "CREATE_USER";
+export const DELETE_USER = "DELETE_USER";
