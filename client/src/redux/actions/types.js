e3896920b3fda9cf0061aa35c375687ee331ab24
Leon <leon.karho.chung@gmail.com>
2020-01-01 17:53:03 -0500

    const

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -3,3 +3,4 @@ export const CURRENT_USER = "CURRENT_USER";
 export const FETCH_EMPLOYEES = "FETCH_EMPLOYEES";
 export const CREATE_USER = "CREATE_USER";
 export const DELETE_USER = "DELETE_USER";
+export const USER_LOGOUT = "USER_LOGOUT";
