dd6f137ac3a405b40f932c1af483b016a80750cd
Leon <leon.karho.chung@gmail.com>
2020-01-01 17:52:35 -0500

    add labels and styling

--- client/src/components/pages/admin/containers/employees/EmployeeDetail.js
+++ client/src/components/pages/admin/containers/employees/EmployeeDetail.js
@@ -3,16 +3,39 @@ import { useDispatch } from "react-redux";
 import { handleDeleteEmployee } from "../../../../../redux/actions/employeesActions";
 
 const EmployeeDetail = ({ location, history }) => {
-  const { firstName, lastName, role, email, _id } = location.state.employee;
+  const {
+    firstName,
+    lastName,
+    role,
+    email,
+    _id,
+    pin
+  } = location.state.employee;
   const dispatch = useDispatch();
   return (
     <div>
-      <h3>{`${firstName} ${lastName}`}</h3>
-      <p>{email}</p>
-      <p>{role}</p>
+      <h1>Employee Detail</h1>
+      <div style={{ margin: "1.5rem 0" }}>
+        <label style={{ marginBottom: "0.5rem" }}>Full name:</label>
+        <h3>{`${firstName} ${lastName}`}</h3>
+      </div>
+      <div style={{ margin: "1.5rem 0" }}>
+        <label style={{ marginBottom: "0.5rem" }}>Email:</label>
+        <p>{email}</p>
+      </div>
+      <div style={{ margin: "1.5rem 0" }}>
+        <label style={{ marginBottom: "0.5rem" }}>Role:</label>
+        <p>{role}</p>
+      </div>
+      <div style={{ margin: "1.5rem 0" }}>
+        <label style={{ marginBottom: "0.5rem" }}>PIN:</label>
+        <p>{pin}</p>
+      </div>
       <button
         onClick={() => {
-          alert("Are you sure you want to delete this employee?");
+          alert(
+            "Are you sure you want to delete this employee from the system?"
+          );
           dispatch(handleDeleteEmployee(_id));
           history.push("/admin/employees");
         }}
