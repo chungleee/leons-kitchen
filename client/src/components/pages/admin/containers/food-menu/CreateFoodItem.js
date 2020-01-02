f6b036a036951fa74ac966133f58e00b0749c10e
Leon <leon.karho.chung@gmail.com>
2020-01-01 20:10:00 -0500

    create food item placeholder comp

---
+++ client/src/components/pages/admin/containers/food-menu/CreateFoodItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Formik } from "formik";
+
+const CreateFoodItem = props => {
+  return <div>this is where food items will be created</div>;
+};
+
+export default CreateFoodItem;
