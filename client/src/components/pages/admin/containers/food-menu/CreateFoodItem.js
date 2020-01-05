60e7c83198694bdc996c1f15cc96661b5fb2ffad
Leon <leon.karho.chung@gmail.com>
2020-01-05 18:08:53 -0500

    implement formik

--- client/src/components/pages/admin/containers/food-menu/CreateFoodItem.js
+++ client/src/components/pages/admin/containers/food-menu/CreateFoodItem.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { Formik } from "formik";
 
+const initialValues = {
+  title: "",
+  category: "",
+  price: ""
+};
 const CreateFoodItem = props => {
-  return <div>this is where food items will be created</div>;
+  return (
+    <div>
+      <h1>Create food item</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          console.log("create food item values", values);
+          // remove set submitting when putting async action
+          actions.setSubmitting(false);
+        }}
+      >
+        {({ handleSubmit, handleChange }) => {
+          return <form onSubmit={handleSubmit}></form>;
+        }}
+      </Formik>
+    </div>
+  );
 };
 
 export default CreateFoodItem;
