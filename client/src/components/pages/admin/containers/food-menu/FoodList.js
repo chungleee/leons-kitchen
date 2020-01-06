5d4288b7d4a7aebafa0c38dfceff8d834be6cffc
Leon <leon.karho.chung@gmail.com>
2020-01-06 16:21:48 -0500

    fetch food items on mount - map through it - render spinner if action still awaits

--- client/src/components/pages/admin/containers/food-menu/FoodList.js
+++ client/src/components/pages/admin/containers/food-menu/FoodList.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import Spinner from "../../../../common/Spinner/Spinner";
+import Button from "../../../../common/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { handleFetchFoodItems } from "../../../../../redux/actions/foodActions";
 
-const FoodList = ({ dummies, match }) => {
+const FoodList = ({ match }) => {
   const { url } = match;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(handleFetchFoodItems());
+  }, []);
+
+  const { food_items } = useSelector(state => {
+    return state.foodItemsState;
+  });
+
+  if (!food_items.length) {
+    return <Spinner />;
+  }
+
   return (
     <div>
-      <Link to={`${url}/create`}>Create food item</Link>
-      {dummies.map(({ id, title, category, price }) => {
+      <Button>
+        <Link to={`${url}/create`}>Create food item</Link>
+      </Button>
+      {food_items.map(({ id, title, category, price }) => {
         return (
           <div key={id}>
             <h2>{title}</h2>
             <p>{category}</p>
-            <p>{price}</p>
+            <p>${price}</p>
           </div>
         );
       })}
