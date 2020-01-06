2c5f917738c3b97a1be55706f49ebe9fd7f2714d
Leon <leon.karho.chung@gmail.com>
2020-01-06 16:22:37 -0500

    food items state

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import employeesReducer from "./employeesReducer";
+import foodReducer from "./foodReducer";
 
 const rootReducer = combineReducers({
   authState: authReducer,
-  employeesState: employeesReducer
+  employeesState: employeesReducer,
+  foodItemsState: foodReducer
 });
 
 export default rootReducer;
