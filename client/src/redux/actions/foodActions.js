b9e9506af432426b6bbc17adea2ba109d041b515
Leon <leon.karho.chung@gmail.com>
2020-01-09 16:25:32 -0500

    delete food item handler

--- client/src/redux/actions/foodActions.js
+++ client/src/redux/actions/foodActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CREATE_FOOD_ITEM, FETCH_FOOD_ITEMS } from "./types";
+import { CREATE_FOOD_ITEM, FETCH_FOOD_ITEMS, DELETE_FOOD_ITEM } from "./types";
 
 export const handleCreateFoodItem = values => {
   return async dispatch => {
@@ -17,6 +17,16 @@ export const handleFetchFoodItems = () => {
     dispatch({
       type: FETCH_FOOD_ITEMS,
       payload: res.data.foods
+    });
+  };
+};
+
+export const handleDeleteFoodItem = id => {
+  return async dispatch => {
+    const res = await axios.delete(`/api/foods/${id}`);
+    dispatch({
+      type: DELETE_FOOD_ITEM,
+      payload: res.data.food
     });
   };
 };
