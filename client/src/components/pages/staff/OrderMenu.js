0f49d00877c8643d5a249ae7aeaadf444ba0f182
Leon <leon.karho.chung@gmail.com>
2020-01-10 18:00:26 -0500

    fetch food items on mount, map Card food items, add item to cart functionality

--- client/src/components/pages/staff/OrderMenu.js
+++ client/src/components/pages/staff/OrderMenu.js
@@ -1,12 +1,60 @@
-import React from "react";
+/**@jsx jsx */
+import { jsx } from "@emotion/core";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { handleFetchFoodItems } from "../../../redux/actions/foodActions";
 import CheckAuth from "../../HOC/CheckAuth";
+import Card from "../../common/Card";
+import Spinner from "../../common/Spinner/Spinner";
 
 const OrderMenu = props => {
-  console.log("props from order menu comp", props);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(handleFetchFoodItems());
+    setLoading(false);
+  }, []);
+
+  const { food_items } = useSelector(state => {
+    return state.foodItemsState;
+  });
+
+  const { cart } = useSelector(state => {
+    return state.foodItemsState;
+  });
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
-    <div>
-      this is the order menu where user orders food - for staffs and (future
-      users)
+    <div css={{ display: "flex", height: "100%" }}>
+      <main
+        css={{
+          width: "75%",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-evenly"
+        }}
+      >
+        {food_items.map(item => {
+          return <Card key={item._id} item={item} />;
+        })}
+      </main>
+      <aside css={{ borderLeft: "1px solid lightgrey", padding: "0 1rem" }}>
+        {cart.length === 0 ? (
+          <p>What are you craving?</p>
+        ) : (
+          cart.map(i => {
+            return (
+              <div key={i._id}>
+                <p>{i.title}</p>
+                <p>${i.price}</p>
+              </div>
+            );
+          })
+        )}
+      </aside>
     </div>
   );
 };
