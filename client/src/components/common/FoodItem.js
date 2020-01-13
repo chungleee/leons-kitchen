bc593441405b717ee6c656db03a0a2deb34d3468
Leon <leon.karho.chung@gmail.com>
2020-01-13 15:03:05 -0500

    add buttons to increment/decrement

--- client/src/components/common/FoodItem.js
+++ client/src/components/common/FoodItem.js
@@ -1,8 +1,10 @@
 /**@jsx jsx */
 import { jsx } from "@emotion/core";
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const FoodItem = ({ title, price, count }) => {
+const FoodItem = ({ title, price, _id }) => {
+  const dispatch = useDispatch();
   return (
     <div
       css={{
@@ -11,18 +13,15 @@ const FoodItem = ({ title, price, count }) => {
         justifyContent: "space-between"
       }}
     >
-      <div css={{ height: "50px", width: "50px" }}>
-        <img
-          css={{ height: "100%", width: "100%", borderRadius: "100193px" }}
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdinnerthendessert.com%2Fwp-content%2Fuploads%2F2016%2F04%2FGeneral-Tsos-Chicken-4-680x453.jpg&f=1&nofb=1"
-          alt={title}
-          sizes=""
-        />
+      <div>
+        <h4>{title}</h4>
+        <p>${price}</p>
       </div>
-      <h4>
-        {title} x {count}
-      </h4>
-      <p>${price * count}</p>
+      <div css={{ display: "flex" }}>
+        <button>-</button>
+        <p>1</p>
+        <button>+</button>
+      </div>
     </div>
   );
 };
