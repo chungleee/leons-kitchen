774e89a5a8a1d9a1c7abcf7f2cc70aa45706eb2f
Leon <leon.karho.chung@gmail.com>
2020-01-23 16:43:34 -0500

    configure upload to imagekit mw

--- server/utils/middlewares.js
+++ server/utils/middlewares.js
@@ -1,5 +1,12 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+  urlEndpoint: "https://ik.imagekit.io/pge4gj9pb"
+});
 
 const errors = {
   logIn: "Please log in.",
@@ -44,4 +51,30 @@ const checkRole = role => async (req, res, next) => {
   }
 };
 
-module.exports = { authenticate, checkRole };
+const uploadToImagekit = async (req, res, next) => {
+  try {
+    if (req.mimetypeError && !req.file) {
+      console.log(req.mimetypeError);
+    } else {
+      await imagekit.upload(
+        {
+          file: req.file.buffer.toString("base64"),
+          fileName: req.file.originalname
+        },
+        (error, result) => {
+          if (error) {
+            console.error(error);
+          } else {
+            req.imagekit_result = result;
+            next();
+          }
+        }
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json(error);
+  }
+};
+
+module.exports = { authenticate, checkRole, uploadToImagekit };
