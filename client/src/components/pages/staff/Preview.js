8fbb42e8900ca0fbd7dadeeb02ba1a581c5b4a0a
Leon <leon.karho.chung@gmail.com>
2020-01-29 13:57:19 -0500

    preview page comp

---
+++ client/src/components/pages/staff/Preview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preview = props => {
+  return (
+    <div>
+      <h1>this is the order preview before payment/checkout</h1>
+    </div>
+  );
+};
+
+export default Preview;
