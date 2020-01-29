72b693f7da08c6b023881122ed35444e7149754f
Leon <leon.karho.chung@gmail.com>
2020-01-29 15:45:22 -0500

    checkout comp

---
+++ client/src/components/pages/staff/Checkout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Checkout = props => {
+  return (
+    <div>
+      <h1>this is the checkout page for stripe etc</h1>
+    </div>
+  );
+};
+export default Checkout;
