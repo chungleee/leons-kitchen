f59c45568af4660a7500890759bc0c1bac5d2071
Leon <leon.karho.chung@gmail.com>
2020-02-02 16:12:23 -0500

    checkout payment form and styling

--- client/src/components/pages/staff/Checkout.js
+++ client/src/components/pages/staff/Checkout.js
@@ -1,10 +1,53 @@
 import React from "react";
+import { useEffect } from "react";
+const stripe = window.Stripe("pk_test_bvyBrZbSRaMYEXOeznlkED2G00aTGv1zec");
+const elements = stripe.elements();
 
 const Checkout = props => {
+  const card = elements.create("card", {
+    classes: {
+      base: "ba br-pill"
+    },
+    style: {
+      base: {
+        fontSize: "16px"
+      }
+    }
+  });
+
+  useEffect(() => {
+    card.mount("#card");
+  }, []);
+
   return (
-    <div>
-      <h1>this is the checkout page for stripe etc</h1>
+    <div className="w-50" style={{ margin: "auto" }}>
+      <h1>this is checkout page</h1>
+      <form className="flex flex-column">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name on card"
+          className="input-reset ba br-pill pl3 h2 mb2 f5"
+        />
+        <div className="flex mb2">
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className="input-reset ba br-pill pl3 h2 w-50 f5"
+          />
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Phone"
+            className="input-reset ba br-pill pl3 h2 w-50 f5"
+          />
+        </div>
+
+        <div style={{ padding: ".5rem 1rem .5rem 1rem" }} id="card"></div>
+      </form>
     </div>
   );
 };
+
 export default Checkout;
