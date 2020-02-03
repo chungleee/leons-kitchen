317610c2208649694954af6845cd5c6fd9967e0c
Leon <leon.karho.chung@gmail.com>
2020-02-03 13:45:06 -0500

    confirm payment handler for form submission + styling

--- client/src/components/pages/staff/Checkout.js
+++ client/src/components/pages/staff/Checkout.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 const stripe = window.Stripe("pk_test_bvyBrZbSRaMYEXOeznlkED2G00aTGv1zec");
 const elements = stripe.elements();
 
+const styles = {
+  input: "input-reset ba br-pill pl3 h2 w-50 f5"
+};
 const Checkout = props => {
+  const { client_secret } = useSelector(state => {
+    return state.paymentState;
+  });
   const card = elements.create("card", {
     classes: {
-      base: "ba br-pill"
+      base: "ba br-pill mb2"
     },
     style: {
       base: {
@@ -15,14 +22,36 @@ const Checkout = props => {
     }
   });
 
+  const handlePay = event => {
+    event.preventDefault();
+
+    stripe
+      .confirmCardPayment(client_secret, {
+        payment_method: {
+          card: card
+        }
+      })
+      .then(result => {
+        console.log("handle pay result", result);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   useEffect(() => {
     card.mount("#card");
   }, []);
 
   return (
-    <div className="w-50" style={{ margin: "auto" }}>
+    <div className="w-90" style={{ margin: "auto" }}>
       <h1>this is checkout page</h1>
-      <form className="flex flex-column">
+      <form
+        className="flex flex-column"
+        onSubmit={event => {
+          handlePay(event);
+        }}
+      >
         <input
           type="text"
           name="name"
@@ -34,17 +63,24 @@ const Checkout = props => {
             type="text"
             name="email"
             placeholder="Email"
-            className="input-reset ba br-pill pl3 h2 w-50 f5"
+            className={styles.input}
           />
           <input
             type="tel"
             name="phone"
             placeholder="Phone"
-            className="input-reset ba br-pill pl3 h2 w-50 f5"
+            className={styles.input}
           />
         </div>
 
         <div style={{ padding: ".5rem 1rem .5rem 1rem" }} id="card"></div>
+
+        <button
+          className="input-reset h2 ba b--black br-pill f5  bg-animate hover-bg-yellow pointer"
+          type="submit"
+        >
+          Pay
+        </button>
       </form>
     </div>
   );
