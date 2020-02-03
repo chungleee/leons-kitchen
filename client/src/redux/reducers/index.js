ddd95a94fadddfd9c77e5075048fa15ed503b648
Leon <leon.karho.chung@gmail.com>
2020-02-03 13:47:01 -0500

    import payment reducer state

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import employeesReducer from "./employeesReducer";
 import foodReducer from "./foodReducer";
+import paymentReducer from "./paymentReducer";
 
 const rootReducer = combineReducers({
   authState: authReducer,
   employeesState: employeesReducer,
-  foodItemsState: foodReducer
+  foodItemsState: foodReducer,
+  paymentState: paymentReducer
 });
 
 export default rootReducer;
