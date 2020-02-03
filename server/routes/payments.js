95a7d0dd43f7ada6d8a245a340b8c7cd2bae4fb6
Leon <leon.karho.chung@gmail.com>
2020-02-03 13:44:20 -0500

    turn amount into acceptable integer and pass into payment intent

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -3,14 +3,14 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 router.post("/create-payment-intent", async (req, res) => {
   try {
-    console.log("req body", req.body);
+    let { amount } = req.body;
+    amount = amount * 100;
 
     const payment_intent = await stripe.paymentIntents.create({
-      amount: 9000,
+      amount,
       currency: "cad"
     });
-    console.log("payment intent", payment_intent);
-
+    console.log(payment_intent);
     return res
       .status(200)
       .json({ success: true, client_secret: payment_intent.client_secret });
