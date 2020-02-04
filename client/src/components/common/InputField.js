71079076bcb607669dfd1b084bb5ab93ed2518a5
Leon <leon.karho.chung@gmail.com>
2020-02-04 12:03:41 -0500

    replace emotioncss with tachyons

--- client/src/components/common/InputField.js
+++ client/src/components/common/InputField.js
@@ -1,46 +1,17 @@
-/** @jsx jsx */
 import React from "react";
-import { jsx } from "@emotion/core";
-import theme from "../../theme";
 
-const styles = {
-  formControl: {
-    display: "flex",
-    flexDirection: "column",
-    marginBottom: "1rem"
-  },
-  label: { marginBottom: "0.5rem" },
-  input: {
-    color: `${theme.text}`,
-    height: "1.5rem",
-    border: `1px solid ${theme.text}`,
-    fontSize: "100%",
-    lineHeight: 1.15,
-    padding: "0.5rem",
-    "&:hover": {
-      backgroundColor: `${theme.color.highlight}`
-    },
-    "&:focus": {
-      backgroundColor: `${theme.color.highlight}`,
-      outline: "none"
-    }
-  },
-  error: {
-    color: `${theme.color.danger}`
-  }
-};
 const InputField = ({ type, name, value, onChange, label, error }) => {
   return (
-    <div css={styles.formControl}>
-      <label css={styles.label}>{label}</label>
+    <div className="flex flex-column mb3">
+      <label className="mb2">{label}</label>
       <input
-        css={styles.input}
+        className="input-reset h2 ba bg-animate hover-bg-yellow"
         type={type}
         name={name}
         value={value}
         onChange={onChange}
       />
-      {error ? <small css={styles.error}>{error}</small> : null}
+      {error ? <small className="red">{error}</small> : null}
     </div>
   );
 };
