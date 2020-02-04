6258b136b49b76ff6513b1170f2efd5890b9e2b6
Leon <leon.karho.chung@gmail.com>
2020-02-04 12:53:10 -0500

    accept cart array from body - get all food items - get subtotal and pass it to payment intent

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -1,16 +1,29 @@
 const router = require("express").Router();
+const Food = require("../models/foodModel");
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 router.post("/create-payment-intent", async (req, res) => {
   try {
-    let { amount } = req.body;
-    amount = amount * 100;
+    const { cart } = req.body;
+    const foodIds = cart.map(item => {
+      return item._id;
+    });
+
+    // get all food items by id
+    const food_items = await Food.find({ _id: { $in: foodIds } });
+
+    // sum up food item prices
+    const subtotal = food_items.reduce((subtotal, current) => {
+      return (subtotal += Number(current.price));
+    }, 0);
 
     const payment_intent = await stripe.paymentIntents.create({
-      amount,
+      amount: subtotal * 100,
       currency: "cad"
     });
+
     console.log(payment_intent);
+
     return res
       .status(200)
       .json({ success: true, client_secret: payment_intent.client_secret });
