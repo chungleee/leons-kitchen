fbe6e88ede35a1a9266629d0bc5696e0d6c0513a
Leon <leon.karho.chung@gmail.com>
2020-02-05 17:06:29 -0500

    combine Preview comp with card checkout + styling

--- client/src/components/pages/staff/Checkout.js
+++ client/src/components/pages/staff/Checkout.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import Preview from "./Preview";
 const stripe = window.Stripe("pk_test_bvyBrZbSRaMYEXOeznlkED2G00aTGv1zec");
 const elements = stripe.elements();
 
 const styles = {
-  input: "input-reset ba br-pill pl3 h2 w-50 f5"
+  input: "input-reset ba br-pill pl3 h2 mb2 f5"
 };
+
+const card = elements.create("card", {
+  classes: {
+    base: "ba br-pill mb2"
+  },
+  style: {
+    base: {
+      fontSize: "16px"
+    }
+  }
+});
+
 const Checkout = ({ history }) => {
   const { client_secret } = useSelector(state => {
     return state.paymentState;
   });
-  const card = elements.create("card", {
-    classes: {
-      base: "ba br-pill mb2"
-    },
-    style: {
-      base: {
-        fontSize: "16px"
-      }
-    }
+
+  const { subtotal } = useSelector(state => {
+    return state.foodItemsState;
   });
 
   const handlePay = async () => {
@@ -41,45 +48,56 @@ const Checkout = ({ history }) => {
   }, []);
 
   return (
-    <div className="w-90" style={{ margin: "auto" }}>
-      <h1>this is checkout page</h1>
-      <form
-        className="flex flex-column"
-        onSubmit={event => {
-          event.preventDefault();
-          handlePay(event);
-        }}
-      >
-        <input
-          type="text"
-          name="name"
-          placeholder="Name on card"
-          className="input-reset ba br-pill pl3 h2 mb2 f5"
-        />
-        <div className="flex mb2">
+    <div className="flex vh-100">
+      <aside className="w-70 br b--light-gray">
+        <Preview />
+      </aside>
+      <div className="w-30" style={{ marginTop: "auto", marginBottom: "auto" }}>
+        <form
+          className="flex flex-column ph3"
+          onSubmit={event => {
+            event.preventDefault();
+            handlePay(event);
+          }}
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name on card"
+            className={styles.input}
+          />
+
           <input
             type="text"
             name="email"
             placeholder="Email"
             className={styles.input}
           />
+
           <input
             type="tel"
             name="phone"
             placeholder="Phone"
             className={styles.input}
           />
-        </div>
 
-        <div style={{ padding: ".5rem 1rem .5rem 1rem" }} id="card"></div>
+          <div style={{ padding: ".5rem 1rem .5rem 1rem" }} id="card"></div>
 
-        <button
-          className="input-reset h2 ba b--black br-pill f5  bg-animate hover-bg-yellow pointer"
-          type="submit"
-        >
-          Pay
-        </button>
-      </form>
+          <div className="mt5 mb3">
+            <h3 className="flex">
+              Total price:{" "}
+              <span style={{ marginLeft: "auto" }}>${subtotal}</span>
+            </h3>
+          </div>
+
+          <button
+            className="input-reset h2 ba b--black br-pill f5  bg-yellow pointer"
+            type="submit"
+          >
+            Place your order
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
