a0dc658e8cca0fd4313ef8a161967a1bbd0941a2
Leon <leon.karho.chung@gmail.com>
2020-02-06 14:59:59 -0500

    send formdata from formik in payment handler func + setup Formik for easy form

--- client/src/components/pages/pos/Checkout.js
+++ client/src/components/pages/pos/Checkout.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Formik } from "formik";
 import Preview from "./Preview";
+import { handleCreateOrder } from "../../../redux/actions/orderActions";
 const stripe = window.Stripe("pk_test_bvyBrZbSRaMYEXOeznlkED2G00aTGv1zec");
 const elements = stripe.elements();
 
@@ -21,21 +23,40 @@ const card = elements.create("card", {
 });
 
 const Checkout = ({ history }) => {
+  const dispatch = useDispatch();
   const { client_secret } = useSelector(state => {
     return state.paymentState;
   });
 
-  const { subtotal } = useSelector(state => {
+  const { subtotal, cart } = useSelector(state => {
     return state.foodItemsState;
   });
 
-  const handlePay = async () => {
+  const { _id } = useSelector(state => {
+    return state.authState.user;
+  });
+
+  const handlePay = async values => {
     try {
       const { paymentIntent } = await stripe.confirmCardPayment(client_secret, {
         payment_method: { card: card }
       });
       console.log("handle pay paymentIntent", paymentIntent);
+
       if (paymentIntent.status === "succeeded") {
+        const food_items_ids = cart.reduce((acc, current) => {
+          return (acc = [...acc, current._id]);
+        }, []);
+
+        const data = {
+          food_items: food_items_ids,
+          price_total: subtotal.toString(),
+          order_for: values.name,
+          payment_type: paymentIntent.payment_method_types[0],
+          order_owner: _id
+        };
+
+        dispatch(handleCreateOrder(data));
         history.push("/staff/invoice");
       }
     } catch (error) {
@@ -53,50 +74,65 @@ const Checkout = ({ history }) => {
         <Preview />
       </aside>
       <div className="w-30" style={{ marginTop: "auto", marginBottom: "auto" }}>
-        <form
-          className="flex flex-column ph3"
-          onSubmit={event => {
-            event.preventDefault();
-            handlePay(event);
+        <Formik
+          initialValues={{ name: "", email: "", phone: "" }}
+          onSubmit={(values, actions) => {
+            handlePay(values);
+            actions.setSubmitting(false);
           }}
         >
-          <input
-            type="text"
-            name="name"
-            placeholder="Name on card"
-            className={styles.input}
-          />
+          {({ values, handleChange, handleSubmit }) => {
+            return (
+              <form className="flex flex-column ph3" onSubmit={handleSubmit}>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  name="name"
+                  value={values.name}
+                  placeholder="Name on card"
+                  className={styles.input}
+                />
 
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            className={styles.input}
-          />
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  name="email"
+                  value={values.email}
+                  placeholder="Email"
+                  className={styles.input}
+                />
 
-          <input
-            type="tel"
-            name="phone"
-            placeholder="Phone"
-            className={styles.input}
-          />
+                <input
+                  onChange={handleChange}
+                  type="tel"
+                  name="phone"
+                  value={values.phone}
+                  placeholder="Phone"
+                  className={styles.input}
+                />
 
-          <div style={{ padding: ".5rem 1rem .5rem 1rem" }} id="card"></div>
+                <div
+                  style={{ padding: ".5rem 1rem .5rem 1rem" }}
+                  id="card"
+                ></div>
 
-          <div className="mt5 mb3">
-            <h3 className="flex">
-              Total price:{" "}
-              <span style={{ marginLeft: "auto" }}>${subtotal}</span>
-            </h3>
-          </div>
+                <div className="mt5 mb3">
+                  <h3 className="flex">
+                    Total price:{" "}
+                    <span style={{ marginLeft: "auto" }}>${subtotal}</span>
+                  </h3>
+                </div>
 
-          <button
-            className="input-reset h2 ba b--black br-pill f5  bg-yellow pointer"
-            type="submit"
-          >
-            Place your order
-          </button>
-        </form>
+                <button
+                  className="input-reset h2 ba b--black br-pill f5  bg-yellow pointer"
+                  type="submit"
+                >
+                  Place your order
+                </button>
+              </form>
+            );
+          }}
+        </Formik>
       </div>
     </div>
   );
