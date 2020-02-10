e7f8ed90d720fa0b5c757562bb7061e082b2417c
Leon <leon.karho.chung@gmail.com>
2020-02-10 14:28:30 -0500

    kitchen comp

---
+++ client/src/components/pages/kitchen/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Kitchen = props => {
+  return (
+    <div style={{ width: "90%", margin: "auto" }}>
+      <div className="flex flex-wrap justify-center">
+        <ul
+          className="ba b--near-black  w-25 ma2"
+          style={{ minHeight: "50vh" }}
+        >
+          some content
+        </ul>
+        <ul
+          className="ba b--near-black  w-25 ma2"
+          style={{ minHeight: "50vh" }}
+        >
+          some content
+        </ul>
+        <ul
+          className="ba b--near-black  w-25 ma2"
+          style={{ minHeight: "50vh" }}
+        >
+          some content
+        </ul>
+        <ul
+          className="ba b--near-black  w-25 ma2"
+          style={{ minHeight: "50vh" }}
+        >
+          some content
+        </ul>
+        <ul
+          className="ba b--near-black  w-25 ma2"
+          style={{ minHeight: "50vh" }}
+        >
+          some content
+        </ul>
+        <ul
+          className="ba b--near-black  w-25 ma2"
+          style={{ minHeight: "50vh" }}
+        >
+          some content
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Kitchen;
