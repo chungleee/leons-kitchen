3f2bc068f19898d9578df1c148fd8b6e0455dbcb
Leon <leon.karho.chung@gmail.com>
2020-02-14 14:08:31 -0500

    init socket connection when component mounts

--- client/src/components/pages/kitchen/index.js
+++ client/src/components/pages/kitchen/index.js
@@ -1,39 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
+// const socket = io.connect("http://localhost/kitchen");
 
 const Kitchen = props => {
+  useEffect(() => {
+    const socket = io("/kitchen");
+
+    socket.on("test", data => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div style={{ width: "90%", margin: "auto" }}>
       <div className="flex flex-wrap justify-center">
-        <ul
-          className="ba b--near-black  w-25 ma2"
-          style={{ minHeight: "50vh" }}
-        >
-          some content
-        </ul>
-        <ul
-          className="ba b--near-black  w-25 ma2"
-          style={{ minHeight: "50vh" }}
-        >
-          some content
-        </ul>
-        <ul
-          className="ba b--near-black  w-25 ma2"
-          style={{ minHeight: "50vh" }}
-        >
-          some content
-        </ul>
-        <ul
-          className="ba b--near-black  w-25 ma2"
-          style={{ minHeight: "50vh" }}
-        >
-          some content
-        </ul>
-        <ul
-          className="ba b--near-black  w-25 ma2"
-          style={{ minHeight: "50vh" }}
-        >
-          some content
-        </ul>
         <ul
           className="ba b--near-black  w-25 ma2"
           style={{ minHeight: "50vh" }}
