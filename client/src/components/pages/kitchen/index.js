6235111f7488d238ced1b1066fbc164010c89f30
Leon <leon.karho.chung@gmail.com>
2020-02-15 17:39:29 -0500

    intialize socket connection when comp mounts to avoid multiple connection creation after every rerender

--- client/src/components/pages/kitchen/index.js
+++ client/src/components/pages/kitchen/index.js
@@ -2,24 +2,35 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import io from "socket.io-client";
 import { handleOrderReceived } from "../../../redux/actions/kitchenActions";
+import { useEffect } from "react";
 
 const Kitchen = props => {
   const dispatch = useDispatch();
-  const socket = io("/kitchen");
 
-  socket.on("new_order", order => {
-    dispatch(handleOrderReceived(order));
+  useEffect(() => {
+    const socket = io("/kitchen");
+    socket.on("new_order", order => {
+      dispatch(handleOrderReceived(order));
+    });
+  }, []);
+
+  const { order_list } = useSelector(state => {
+    return state.kitchenState;
   });
 
   return (
     <div style={{ width: "90%", margin: "auto" }}>
       <div className="flex flex-wrap justify-center">
-        <ul
-          className="ba b--near-black  w-25 ma2"
-          style={{ minHeight: "50vh" }}
-        >
-          some content
-        </ul>
+        {order_list.map(order => {
+          return (
+            <ul
+              className="ba b--near-black  w-25 ma2"
+              style={{ minHeight: "50vh" }}
+            >
+              some content
+            </ul>
+          );
+        })}
       </div>
     </div>
   );
