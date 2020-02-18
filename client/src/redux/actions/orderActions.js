704765924b5435737a6f261f75d3a6871a6ec379
Leon <leon.karho.chung@gmail.com>
2020-02-18 16:39:31 -0500

    fetch orders handler

--- client/src/redux/actions/orderActions.js
+++ client/src/redux/actions/orderActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CREATE_ORDER } from "./types";
+import { CREATE_ORDER, FETCH_ORDERS } from "./types";
 
 export const handleCreateOrder = data => {
   return async dispatch => {
@@ -12,6 +12,23 @@ export const handleCreateOrder = data => {
         });
       }
       return result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const handleFetchOrders = () => {
+  return async dispatch => {
+    try {
+      const result = await axios.get("/api/orders");
+      console.log("fetch orders", result);
+      if (result.data.success) {
+        dispatch({
+          type: FETCH_ORDERS,
+          payload: result.data.data
+        });
+      }
     } catch (error) {
       console.error(error);
     }
