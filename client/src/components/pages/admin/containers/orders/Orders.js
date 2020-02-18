0c2e50321d367d05243b436c920a9fcbb2e907d3
Leon <leon.karho.chung@gmail.com>
2020-02-18 16:39:12 -0500

    orders page comp: fetch orders on mount - map each order

--- client/src/components/pages/admin/containers/orders/Orders.js
+++ client/src/components/pages/admin/containers/orders/Orders.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { handleFetchOrders } from "../../../../../redux/actions/orderActions";
+import Spinner from "../../../../common/Spinner/Spinner";
 
 const Orders = props => {
-  return <div>this is the Orders route</div>;
+  const dispatch = useDispatch();
+  const { completed_orders } = useSelector(state => {
+    return state.orderState;
+  });
+  useEffect(() => {
+    dispatch(handleFetchOrders());
+  }, []);
+
+  if (!completed_orders.length) {
+    return <Spinner />;
+  }
+
+  return (
+    <div>
+      {completed_orders.map(order => {
+        return (
+          <div key={order._id} className="flex justify-between pv3">
+            <div className="tc w-25">
+              <p>{order.order_id}</p>
+            </div>
+            <div className="tc w-25">
+              <p className="ttc">{order.order_for.name}</p>
+            </div>
+            <div className="tc w-25">
+              <p>{order.payment_type}</p>
+            </div>
+            <div className="tc w-25">
+              <p>${order.price_total}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Orders;
